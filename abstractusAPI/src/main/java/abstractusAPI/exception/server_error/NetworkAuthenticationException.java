f079ad89a472d109f4257bec9b1089a1ecd4418a
Sam <unpluggedsam990@gmail.com>
2022-10-05 22:57:25 -0700

    Update NetworkAuthenticationException.java

--- abstractusAPI/src/main/java/abstractusAPI/exception/server_error/NetworkAuthenticationException.java
+++ abstractusAPI/src/main/java/abstractusAPI/exception/server_error/NetworkAuthenticationException.java
@@ -5,8 +5,8 @@ import abstractusAPI.exception.AbstractusException;
 /**
  * <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/511>Indicates that the client needs to authenticate to gain network access.</a>"
  */
-public class NetowrkAuthenticationRequiredException extends AbstractusException {
-    public NetowrkAuthenticationRequiredException(String message) {
+public class NetworkAuthenticationException extends AbstractusException {
+    public NetworkAuthenticationException(String message) {
         super(message, 511);
     }
 }
