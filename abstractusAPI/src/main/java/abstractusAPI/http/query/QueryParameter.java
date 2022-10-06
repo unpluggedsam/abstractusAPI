958a7c2862b01070defaaada8f1b95d3012a33c5
Sam <unpluggedsam990@gmail.com>
2022-10-05 23:39:29 -0700

    Update QueryParameter.java

--- abstractusAPI/src/main/java/abstractusAPI/http/query/QueryParameter.java
+++ abstractusAPI/src/main/java/abstractusAPI/http/query/QueryParameter.java
@@ -3,11 +3,11 @@ package abstractusAPI.http.query;
 /**
  * Keeps track of the parameters being passed into the request to the API.
  *
- * @param type  The key of the parameters being passed into the request.
- * @param value The value of the parameters being passed into the request.
+ * @param key  The key of the parameter being passed into the request.
+ * @param value The value of the parameter being passed into the request.
  */
 
-public record QueryParameter(QueryParameterTypes type, String value) {
+public record QueryParameter(String key, String value) {
 
     @Override
     public boolean equals(Object o) {
@@ -17,7 +17,7 @@ public record QueryParameter(QueryParameterTypes type, String value) {
         }
 
         QueryParameter other = (QueryParameter) o;
-        return type.equals(other.type()) && value.equals(other.value());
+        return key().equals(other.key()) && value().equals(other.value());
     }
 
 }
