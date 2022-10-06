0e4382abfa0f248cb875881a779535663dd61608
Sam <unpluggedsam990@gmail.com>
2022-10-05 23:39:35 -0700

    Create Query.java

---
+++ abstractusAPI/src/main/java/abstractusAPI/http/query/Query.java
@@ -0,0 +1,72 @@
+package abstractusAPI.http.query;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.Request;
+
+
+/**
+ * A request to a Web API.
+ */
+public class Query {
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    private final Optional<QueryParameter[]> queryParameterList;
+    private final URL url;
+
+    /**
+     * Begins building a new HTTP request that can be used to make the query.
+     * <p>
+     * The returned {@link HttpUrl.Builder builder} will always have at least its
+     * <code>URL</code> configured, but may also include headers, a body, or
+     * any other information relevant to the query.
+     *
+     * @return A minimally-configured HTTP request with only the information relevant to the query
+     * itself.
+     * @apiNote A builder is returned instead of a finalized {@link Request request} so that the
+     * caller can attach any additional information it needs to the request, such as a user-agent or
+     * API credentials.
+     */
+    public HttpUrl.Builder createRequest() {
+        HttpUrl.Builder builder = Objects.requireNonNull(HttpUrl.get(url)
+                .newBuilder());
+        queryParameterList.ifPresent(queryParameters -> Arrays.stream(queryParameters).toList()
+                .forEach(parameter -> builder.addQueryParameter(parameter.key(),
+                        parameter.value())));
+        return builder;
+    }
+
+    public Query(URL url, QueryParameter... queryParameterList) {
+        this.queryParameterList = Optional.ofNullable(queryParameterList);
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+
+        Query other = (Query) o;
+
+        if (url.equals(other.url)) {
+            if (queryParameterList.isEmpty() && other.queryParameterList.isEmpty()) {
+                return true;
+            }
+            if (queryParameterList.isEmpty() || other.queryParameterList.isEmpty()) {
+                return false;
+            }
+
+            return Arrays.equals(queryParameterList.get(), other.queryParameterList.get());
+        } else {
+            return false;
+        }
+
+    }
+}
