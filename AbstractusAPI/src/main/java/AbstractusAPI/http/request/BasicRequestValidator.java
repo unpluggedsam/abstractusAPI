ecbbab26ad0db20c2e210d7f1388a1e969778b28
Sam <unpluggedsam990@gmail.com>
2024-06-02 13:04:36 -0700

    Refactor Exceptions for readibility

--- AbstractusAPI/src/main/java/AbstractusAPI/http/request/BasicRequestValidator.java
+++ AbstractusAPI/src/main/java/AbstractusAPI/http/request/BasicRequestValidator.java
@@ -1,11 +1,14 @@
 package AbstractusAPI.http.request;
 
+import AbstractusAPI.exception.AbstractusException;
 import AbstractusAPI.exception.client_error.*;
 import AbstractusAPI.exception.server_error.*;
-import AbstractusAPI.exception.server_error.HTTPVersionNotSupportedException;
 
 import okhttp3.Response;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Basic implementation of {@link RequestValidator}. Handles most
@@ -13,65 +16,79 @@ import org.json.JSONObject;
  */
 public class BasicRequestValidator implements RequestValidator {
 
+    private static final Map<Integer, Class<? extends AbstractusException>> CLIENT_ERROR_MAP = new HashMap<>();
+    private static final Map<Integer, Class<? extends AbstractusException>> SERVER_ERROR_MAP = new HashMap<>();
+
+    static {
+        // Client error mappings
+        CLIENT_ERROR_MAP.put(400, BadRequestException.class);
+        CLIENT_ERROR_MAP.put(401, UnauthorizedException.class);
+        CLIENT_ERROR_MAP.put(402, PaymentRequiredException.class);
+        CLIENT_ERROR_MAP.put(403, ForbiddenException.class);
+        CLIENT_ERROR_MAP.put(404, NotFoundException.class);
+        CLIENT_ERROR_MAP.put(405, MethodNotAllowedException.class);
+        CLIENT_ERROR_MAP.put(406, NotAcceptableException.class);
+        CLIENT_ERROR_MAP.put(407, ProxyAuthenticationRequiredException.class);
+        CLIENT_ERROR_MAP.put(408, RequestTimeoutException.class);
+        CLIENT_ERROR_MAP.put(409, ConflictException.class);
+        CLIENT_ERROR_MAP.put(410, GoneException.class);
+        CLIENT_ERROR_MAP.put(411, LengthRequiredException.class);
+        CLIENT_ERROR_MAP.put(412, PreconditionFailedException.class);
+        CLIENT_ERROR_MAP.put(413, PayloadTooLargeException.class);
+        CLIENT_ERROR_MAP.put(414, URITooLongException.class);
+        CLIENT_ERROR_MAP.put(415, UnsupportedMediaTypeException.class);
+        CLIENT_ERROR_MAP.put(416, RangeNotSatisfiableException.class);
+        CLIENT_ERROR_MAP.put(417, ExpectationFailedException.class);
+        CLIENT_ERROR_MAP.put(418, TeapotException.class);
+        CLIENT_ERROR_MAP.put(419, MisdirectedRequestException.class);
+        CLIENT_ERROR_MAP.put(422, UnprocessableEntityException.class);
+        CLIENT_ERROR_MAP.put(423, LockedException.class);
+        CLIENT_ERROR_MAP.put(424, FailedDependencyException.class);
+        CLIENT_ERROR_MAP.put(425, TooEarlyException.class);
+        CLIENT_ERROR_MAP.put(426, UpgradeRequiredException.class);
+        CLIENT_ERROR_MAP.put(428, PreconditionRequiredException.class);
+        CLIENT_ERROR_MAP.put(429, RateLimitingException.class);
+        CLIENT_ERROR_MAP.put(431, RequestHeaderFieldsTooLargeException.class);
+        CLIENT_ERROR_MAP.put(451, UnavailableForLegalReasonsException.class);
+
+        // Server error mappings
+        SERVER_ERROR_MAP.put(500, InternalServerErrorException.class);
+        SERVER_ERROR_MAP.put(501, NotImplementedException.class);
+        SERVER_ERROR_MAP.put(502, BadGatewayException.class);
+        SERVER_ERROR_MAP.put(503, ServiceUnavailableException.class);
+        SERVER_ERROR_MAP.put(504, GatewayTimeoutException.class);
+        SERVER_ERROR_MAP.put(505, HTTPVersionNotSupportedException.class);
+        SERVER_ERROR_MAP.put(506, VariantAlsoNegotiatesException.class);
+        SERVER_ERROR_MAP.put(507, InsufficientStorageException.class);
+        SERVER_ERROR_MAP.put(508, LoopDetectedException.class);
+        SERVER_ERROR_MAP.put(510, NotExtendedException.class);
+        SERVER_ERROR_MAP.put(511, NetworkAuthenticationException.class);
+    }
 
     @Override
     public boolean validate(Response response, JSONObject returnObject) {
         if (response.isSuccessful()) {
             return true;
         } else {
-            String failString = returnObject.getString("cause");
+            String failString = returnObject.optString("cause", "Unknown error");
+            int statusCode = response.code();
 
-            switch (response.code()) {
+            if (CLIENT_ERROR_MAP.containsKey(statusCode)) {
+                throwException(CLIENT_ERROR_MAP.get(statusCode), failString);
+            } else if (SERVER_ERROR_MAP.containsKey(statusCode)) {
+                throwException(SERVER_ERROR_MAP.get(statusCode), failString);
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
 
-                // client errors
-                case 400 -> throw new BadRequestException(failString);
-                case 401 -> throw new UnauthorizedException(failString);
-                case 402 -> throw new PaymentRequiredException(failString);
-                case 403 -> throw new ForbiddenException(failString);
-                case 404 -> throw new NotFoundException(failString);
-                case 405 -> throw new MethodNotAllowedException(failString);
-                case 406 -> throw new NotAcceptableException(failString);
-                case 407 -> throw new ProxyAuthenticationRequiredException(failString);
-                case 408 -> throw new RequestTimeoutException(failString);
-                case 409 -> throw new ConflictException(failString);
-                case 410 -> throw new GoneException(failString);
-                case 411 -> throw new LengthRequiredException(failString);
-                case 412 -> throw new PreconditionFailedException(failString);
-                case 413 -> throw new PayloadTooLargeException(failString);
-                case 414 -> throw new URITooLongException(failString);
-                case 415 -> throw new UnsupportedMediaTypeException(failString);
-                case 416 -> throw new RangeNotSatisfiableException(failString);
-                case 417 -> throw new ExpectationFailedException(failString);
-                case 418 -> throw new TeapotException(failString);
-                case 419 -> throw new MisdirectedRequestException(failString);
-                case 422 -> throw new UnprocessableEntityException(failString);
-                case 423 -> throw new LockedException(failString);
-                case 424 -> throw new FailedDependencyException(failString);
-                case 425 -> throw new TooEarlyException(failString);
-                case 426 -> throw new UpgradeRequiredException(failString);
-                case 428 -> throw new PreconditionRequiredException(failString);
-                case 429 -> throw new RateLimitingException(failString);
-                case 431 -> throw new RequestHeaderFieldsTooLargeException(failString);
-                case 451 -> throw new UnavailableForLegalReasonsException(failString);
-
-                // server errors
-                case 500 -> throw new InternalServerErrorException(failString);
-                case 501 -> throw new NotImplementedException(failString);
-                case 502 -> throw new BadGatewayException(failString);
-                case 503 -> throw new ServiceUnavailableException(failString);
-                case 504 -> throw new GatewayTimeoutException(failString);
-                case 505 -> throw new HTTPVersionNotSupportedException(failString);
-                case 506 -> throw new VariantAlsoNegotiatesException(failString);
-                case 507 -> throw new InsufficientStorageException(failString);
-                case 508 -> throw new LoopDetectedException(failString);
-                case 510 -> throw new NotExtendedException(failString);
-                case 511 -> throw new NetworkAuthenticationException(failString);
-
-                default -> {
-                    return false;
-                }
-
+        private void throwException (Class <? extends AbstractusException> exceptionClass, String message){
+            try {
+                throw exceptionClass.getConstructor(String.class).newInstance(message);
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException("Failed to instantiate exception for status code", e);
             }
         }
     }
-}
